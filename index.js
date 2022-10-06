f45e40d883dab3a6d6bae887901c0524451f7a22
Yifan Shi <stevenls1118@msn.com>
2022-10-06 17:29:40 -0600

    sum function to add from a to b

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (toN === fromN) {
+    return toN;
+  } else if (fromN < toN) {  // toN > fromN
+    return fromN + sum(fromN + 1, toN); // toN + sum(fromN, toN - 1);
+  } else {
+    return "toN should be greater or equal to fromN";
+  }
 }
 
 module.exports = sum;
